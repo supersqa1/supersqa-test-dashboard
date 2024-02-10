652d89726f15eaed11807737fa7c4ec9b93b9dbe
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2024-02-10 07:27:48 -0800

    added log output seeting

--- automationdashboard/__init__.py
+++ automationdashboard/__init__.py
@@ -1,8 +1,14 @@
 
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 import os
 
 app = Flask(__name__)
+
+# Sett logging config
+
+
 
 # set configs
 app.config['DATA_STORAGE'] = "database"  # Options are 'database' or 'file'.
@@ -16,7 +22,27 @@ app.config['DB_USER'] = os.environ.get("DB_USER")
 app.config['DB_PASSWORD'] = os.environ.get("DB_PASSWORD")
 app.config['DB_PORT'] = os.environ.get("DB_PORT")
 
+APP_INSTANCE_DIR = os.environ.get("APP_INSTANCE_DIR")
 
+# Set log file path
+log_file_path = f'{APP_INSTANCE_DIR}/logfile.log'
+
+# Create a file handler object
+file_handler = RotatingFileHandler(log_file_path, maxBytes=10240, backupCount=10)
+
+# Set the log format
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+))
+
+# Set the log level - change to logging.ERROR for only errors
+file_handler.setLevel(logging.DEBUG)
+
+# Add the handler to the Flask app's logger
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
+
+app.logger.info('Application startup')
 
 
 
