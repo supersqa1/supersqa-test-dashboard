05ecd5f60d6ebe99c133c1e4f02722ef30c9e7f9
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2025-04-16 23:00:47 -0700

    implementing actions ci

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,12 @@ set -x
 # Deploys the application to the provided ip address (Ubuntu server on digitalocean (VPS))
 # Expects certain variables to be set
 
-
 IP=$REMOTE_HOST_IP
 REMOTE_SSH_USER=root
-
+SSH_KEY_FILE=~/.ssh/deploy_key
 
 # set database info from GitLab's variables
 if [ ${ENVIRONMENT} == 'staging' ]; then
-
     DB_HOST=$STAGING_DB_HOST
     DB_PORT=$STAGING_DB_PORT
     DB_USER=$STAGING_DB_USER
@@ -25,16 +23,14 @@ else
   exit 1
 fi
 
-
-
 # TODO: just do one ssh connection and multiple commands
-chmod 400 ${DRP3_KEY}
-ssh -o StrictHostKeyChecking=no -i ${DRP3_KEY} "$REMOTE_SSH_USER@$IP" "mkdir -p ${APP_INSTANCE_DIR}"
-scp -o StrictHostKeyChecking=no -i ${DRP3_KEY} -r ./automationdashboard $REMOTE_SSH_USER@$IP:${APP_INSTANCE_DIR}
-scp -o StrictHostKeyChecking=no -i ${DRP3_KEY} -r ./requirements.txt $REMOTE_SSH_USER@$IP:${APP_INSTANCE_DIR}
-ssh -o StrictHostKeyChecking=no -i $DRP3_KEY "$REMOTE_SSH_USER@$IP" "python3 -m venv ${VIRTUAL_ENVIRONMENT}"
-ssh -o StrictHostKeyChecking=no -i $DRP3_KEY "$REMOTE_SSH_USER@$IP" "${VIRTUAL_ENVIRONMENT}/bin/python -m pip install -r ${APP_INSTANCE_DIR}/requirements.txt"
-ssh -o StrictHostKeyChecking=no -i $DRP3_KEY "$REMOTE_SSH_USER@$IP" "cd ${APP_INSTANCE_DIR}/automationdashboard \
+chmod 400 ${SSH_KEY_FILE}
+ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} "$REMOTE_SSH_USER@$IP" "mkdir -p ${APP_INSTANCE_DIR}"
+scp -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} -r ./automationdashboard $REMOTE_SSH_USER@$IP:${APP_INSTANCE_DIR}
+scp -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} -r ./requirements.txt $REMOTE_SSH_USER@$IP:${APP_INSTANCE_DIR}
+ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} "$REMOTE_SSH_USER@$IP" "python3 -m venv ${VIRTUAL_ENVIRONMENT}"
+ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} "$REMOTE_SSH_USER@$IP" "${VIRTUAL_ENVIRONMENT}/bin/python -m pip install -r ${APP_INSTANCE_DIR}/requirements.txt"
+ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_FILE} "$REMOTE_SSH_USER@$IP" "cd ${APP_INSTANCE_DIR}/automationdashboard \
 && export PYTHONPATH=${APP_INSTANCE_DIR} \
 && export APP_INSTANCE_DIR=${APP_INSTANCE_DIR} \
 && export ENVIRONMENT=${ENVIRONMENT} \
